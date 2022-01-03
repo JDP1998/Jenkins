903b646dc0a5e7ce2feeb2a67a06cb0e48b48793
JDP1998 <89901594+JDP1998@users.noreply.github.com>
2022-01-03 12:23:19 +0100

    Weihnachten_Update

--- src/Jenkins/dlg_Weihnachten.java
+++ src/Jenkins/dlg_Weihnachten.java
@@ -7,7 +7,7 @@ public class dlg_Weihnachten {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GregorianCalendar heute = new GregorianCalendar();
-		GregorianCalendar weihnachten = new GregorianCalendar(2021, 12, 24);
+		GregorianCalendar weihnachten = new GregorianCalendar(2022, 12, 24);
 		long differenz = weihnachten.getTimeInMillis()-heute.getTimeInMillis();
 		int tage;
 		tage=(int)(differenz/(1000*60*60*24))-30;
